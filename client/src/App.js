5b5eb93872ba3708ddf19e5c5ef5b3ea6d39ccae
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-04-14 17:09:12 +0000

    Set up front end

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+    return (
+        <>
+            <h1>This is my App component</h1>
+        </>
+    )
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { GlobalProvider } from './context/GlobalState';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <GlobalProvider>
+    <App />
+  </GlobalProvider>
+);
